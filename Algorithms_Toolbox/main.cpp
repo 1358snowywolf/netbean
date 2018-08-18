61da60d7186287ff49e69b388be77179e67376b3
test account <abc@example.com>
2018-08-18 10:42:18 -0700

    another commit

--- Algorithms_Toolbox/main.cpp
+++ Algorithms_Toolbox/main.cpp
@@ -111,6 +111,7 @@ public:
 
 class CoinExchange{
 public:
+
     int coinExchange(int change, vector<int> coins){
         if(change == 0){
             return 0;
@@ -124,7 +125,7 @@ public:
                 int current_coins = INT_MAX;
                 int cent_value = 0;
                 for(int d = 0; d < coins.size(); d++){
-                    if(i - coins.at(d) >= 0 && answer[i - d] != -1){  //error
+                    if(i - coins.at(d) >= 0 && answer[i - coins[d]] != -1){  //error
                         current_coins = min(current_coins, answer[i - d] + 1);
                         cent_value = coins[d];
                     }
@@ -141,18 +142,82 @@ public:
                 } 
             }
             
-            return answer[change - 1];
+            return answer[change];
         }
     }
+    
+    int recursive(int change, vector<int> coins, int used){
+        if(change == 0){
+            return 0;
+        }
+        for(int i = 0; i < coins.size(); i++){
+            if(change - coins[i] >= 0){
+                int rec = recursive(change - coins[i], coins, used + 1);
+                
+                if(rec == -1){
+                    continue;
+                }
+                else{
+                    used = min(used, used + rec);
+                    change -= coins[i];
+                }
+            }
+            
+            if(i + 1 == coins.size()){
+                return -1;
+            }
+        }
+
+        return used;
+    }
+    
+    int recursiveII(int change, const vector<int>& coins) {
+        if (change == 0) {
+            return value[0];
+        }
+        if (change < 0) {
+            return INT_MAX;
+        }
+        int res = INT_MAX;
+        
+        for (auto coin = coins.begin(); coin != coins.end(); coin++) {
+            if ((change - *coin) >= 0 && value[change - *coin] != INT_MAX) {
+                res= min(res, value[change - *coin]) + 1;
+            } 
+            else {
+                int tmp = recursiveII(change - *coin, coins);
+                if (tmp != INT_MAX) {   //prevent integer overflow
+                    tmp ++;
+                }
+                res = min (res, tmp);
+            }
+            
+ 
+        }
+        if (res < value[change]) {
+             value[change] = res;
+         }
+        return res;
+    }
+    
+    int setValue(int size) {
+        value = vector<int>(size + 1, INT_MAX);
+        value[0] = 0;
+    }
+private: 
+    vector<int> value;
 };
 
 int main(int argc, char** argv) {
     
-    int change = 7;
-    int array[] = {2};
+    int change = 11;
+    int array[] = {9, 6, 5, 1};
     vector<int> coins (array, array + sizeof(array) / sizeof(int));
     CoinExchange* ce = new CoinExchange();
-    auto thing = ce->coinExchange(change, coins);
-    
+    ce->setValue(change);
+    auto thing = ce->recursiveII(change, coins);
+    cout <<thing <<endl;
+    cout << LONG_MAX << endl;
+    cout << LONG_MAX + 1;
     return 0;
 }
